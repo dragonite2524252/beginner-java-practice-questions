f72c3fe7d4df9d1501e57383a9237a3448e9c427
dragonite2524252 <35555605+dragonite2524252@users.noreply.github.com>
2019-08-04 23:45:49 -0400

    Update Solution.java

--- task10/task1001/Solution.java
+++ task10/task1001/Solution.java
@@ -7,8 +7,8 @@ Task No. 1 about integer type conversions
 
 public class Solution {
     public static void main(String[] args) {
-        int a = 0;
-        int b = (byte) a + 46;
+        int zero = 0;
+        int zero_plus_val = (byte) a + 46;
         byte c = (byte) (a * b);
         double f = (char) 1234.15;
         long d = (int) (a + f / c + b);
